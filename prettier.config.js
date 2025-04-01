c6cfeb71a182cbd86b6e7eaff85426d8a61842c6
RealSquareJL <juliallukaszewska@gmail.com>
2025-04-01 16:26:47 +0200

    chore: add ESLint and Prettier configuration, update dependencies, and refactor project structure

---
+++ prettier.config.js
@@ -0,0 +1,9 @@
+// prettier.config.js
+export default {
+    singleQuote: true,       // używaj ' zamiast "
+    semi: false,             // brak średników
+    tabWidth: 2,             // 2 spacje
+    trailingComma: 'es5',    // przecinki w obiektach, tablicach, itd.
+    bracketSpacing: true,    // { a: 1 } zamiast {a:1}
+  }
+  
