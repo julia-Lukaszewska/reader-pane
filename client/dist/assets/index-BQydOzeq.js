521e4efbb1f5dece7ba8b693f43d68affbc9fbb5
RealSquareJL <juliallukaszewska@gmail.com>
2025-05-24 19:46:38 +0200

    fix(env): updated API URL to include /api/books

---
+++ client/dist/assets/index-BQydOzeq.js
@@ -0,0 +1,7 @@
+import{j as e,O as o,d as a}from"./index-CbYdQxyi.js";import{Header as r}from"./index-B2Lw4VGN.js";const t=a.div`
+  display: flex;
+  flex-direction: column;
+  height: 100vh;  
+  width: 100vw;
+  background: var(--gradient-blue-clear);
+`,l=()=>e.jsxDEV(t,{children:[e.jsxDEV(r,{},void 0,!1,{fileName:"C:/Reader-App/client/src/layout/HomeLayout/HomeLayout.jsx",lineNumber:33,columnNumber:7},void 0),e.jsxDEV(o,{},void 0,!1,{fileName:"C:/Reader-App/client/src/layout/HomeLayout/HomeLayout.jsx",lineNumber:34,columnNumber:7},void 0)]},void 0,!0,{fileName:"C:/Reader-App/client/src/layout/HomeLayout/HomeLayout.jsx",lineNumber:32,columnNumber:5},void 0);export{l as default};
