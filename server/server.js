ead30bfe70a655c3b83dd1a282040854c6af7961
RealSquareJL <juliallukaszewska@gmail.com>
2025-05-24 12:30:31 +0200

    fix: add Vercel URL to CORS origin

--- server/server.js
+++ server/server.js
@@ -68,7 +68,8 @@ app.use(
     origin: [
       'http://localhost:5173',
       'http://localhost:5174',
-      'https://reader-pane-frontend.onrender.com'
+      'https://reader-pane-frontend.onrender.com',
+      'https://reader-pane.vercel.app',
     ],
     credentials: true,
   })
