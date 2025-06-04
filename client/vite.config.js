f2cd4a184a5a9040d59e237a8de08065e6fb7916
RealSquareJL <juliallukaszewska@gmail.com>
2025-06-04 20:56:12 +0200

    config

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,8 +1,13 @@
+import { fileURLToPath } from 'url'
+import { dirname, resolve } from 'path'
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
-import { resolve } from 'path'
 import fs from 'fs'
 import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill'
+
+// -------- __dirname in ESM --------
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 export default defineConfig(({ mode }) => {
   // ------------- env -------------
