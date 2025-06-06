ecc1270fa7137cf166c17dce5e6b035a874aab07
RealSquareJL <juliallukaszewska@gmail.com>
2025-06-06 21:19:11 +0200

    api

--- client/src/modules/reader/hooks/useEnsureBookFileUrl.js
+++ client/src/modules/reader/hooks/useEnsureBookFileUrl.js
@@ -18,7 +18,7 @@ const useEnsureBookFileUrl = (bookId) => {
   const book = cachedBook || fetchedBook
 
   if (!book?.file?.filename) return null
-  return `/books/public/file/${book.file.filename}`
+  return `/api/books/public/file/${book.file.filename}`
 }
 
 export default useEnsureBookFileUrl
