550be930c107c13ba4ff3c3fc91e05ed8a392d9d
RealSquareJL <juliallukaszewska@gmail.com>
2025-06-08 02:35:49 +0200

    12

--- server/middlewares/authJwt.js
+++ server/middlewares/authJwt.js
@@ -2,6 +2,6 @@
  * @file middlewares/authJwt.js
  * @description Sprawdza i dekoduje JWT z nagłówka Authorization
  */
-import passport from '../config/passport.js';
+import passport from 'passport';
 
 export default passport.authenticate('jwt', { session: false });
