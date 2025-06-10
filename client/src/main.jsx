8fb511507721e202f24430102110e00045729ffe
RealSquareJL <juliallukaszewska@gmail.com>
2025-06-10 21:44:13 +0200

    6

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,7 +2,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import AppProvider from './providers/AppProvider'
-import App from '@/App'
+import App from './App'
 
 
 import * as pdfjsLib from 'pdfjs-dist'
