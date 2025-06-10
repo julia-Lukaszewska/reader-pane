c87973ad227e3e2b25bf8dbfec87664ef1015236
RealSquareJL <juliallukaszewska@gmail.com>
2025-06-10 05:57:47 +0200

    correct6

--- server/config/sentry.server.js
+++ server/config/sentry.server.js
@@ -1,14 +1,14 @@
 // config/sentry.server.js
-import * as Sentry from '@sentry/node';
+import { init, Handlers } from '@sentry/node';
 import dotenv from 'dotenv';
 dotenv.config();
 
-Sentry.init({
+init({
   dsn: process.env.SENTRY_DSN,
   environment: process.env.NODE_ENV || 'development',
   release: 'reader-backend@1.0.0',
   tracesSampleRate: 1.0,
 });
 
-export const sentryRequestHandler = Sentry.Handlers.requestHandler();
-export const sentryErrorHandler   = Sentry.Handlers.errorHandler();
+export const sentryRequestHandler = Handlers.requestHandler();
+export const sentryErrorHandler  = Handlers.errorHandler();
